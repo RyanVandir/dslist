53c56a5ba4e47e4b51737ffcc7c560362071d3f6
RyanVandir <r.vandir@outlook.com>
2024-11-11 17:41:39 -0300

    Project created

---
+++ src/test/java/com/example/dllist/DllistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dllist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DllistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

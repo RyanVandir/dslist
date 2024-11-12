27e34b31c32bab91d72c428423bfccb6b58ee60e
RyanVandir <r.vandir@outlook.com>
2024-11-12 19:13:23 -0300

    get by Id

--- src/main/java/com/example/dllist/controller/GameController.java
+++ src/main/java/com/example/dllist/controller/GameController.java
@@ -1,9 +1,11 @@
 package com.example.dllist.controller;
 
+import com.example.dllist.dto.GameDTO;
 import com.example.dllist.dto.GameMinDTO;
 import com.example.dllist.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +21,11 @@ public class GameController {
     @GetMapping
     public List<GameMinDTO> findAll() {
         return gameService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public GameDTO findById(@PathVariable Long id) {
+        GameDTO result =  gameService.findById(id);
+        return result;
     }
 }
--- src/main/java/com/example/dllist/service/GameService.java
+++ src/main/java/com/example/dllist/service/GameService.java
@@ -1,10 +1,12 @@
 package com.example.dllist.service;
 
+import com.example.dllist.dto.GameDTO;
 import com.example.dllist.dto.GameMinDTO;
 import com.example.dllist.entities.GameEntity;
 import com.example.dllist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -13,6 +15,11 @@ public class GameService {
 
     @Autowired
     private GameRepository gameRepository;
+
+    public GameDTO findById(@PathVariable Long id) {
+        GameEntity result = gameRepository.findById(id).get();
+        return new GameDTO(result);
+    }
 
     public List<GameMinDTO> findAll() {
         List<GameEntity> games = gameRepository.findAll();
